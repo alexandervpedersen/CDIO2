54085b1a379006984e7064425bce54f3f9662912
AoschkA <jonaspraem@gmail.com>
2014-10-28 11:19:27 +0100

    Account class added

---
+++ src/Game/Account.java
@@ -0,0 +1,19 @@
+package Game;
+
+public class Account {
+		
+	private int balance;
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public void addBalance(int add) {
+		this.balance = this.balance + add;
+	}
+
+}
